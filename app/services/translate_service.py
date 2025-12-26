b189f6001601385a49f27933d004bbd4c75f75f7
Aidar <nasirovaidar11@gmail.com>
2025-12-26 16:45:03 +0600

    google api

--- app/services/translate_service.py
+++ app/services/translate_service.py
@@ -1,20 +1,17 @@
 import os
-from dotenv import load_dotenv
+import json
 from google.cloud import translate_v2 as translate
 from google.oauth2 import service_account
 
-load_dotenv()
+# Load JSON from environment variable
+credentials_info = json.loads(os.environ["GOOGLE_CREDENTIALS_JSON"])
 
-# Path to your JSON key
-key_path = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
+# Create credentials object
+credentials = service_account.Credentials.from_service_account_info(credentials_info)
 
-# Load credentials
-credentials = service_account.Credentials.from_service_account_file(key_path)
-
-# Initialize client with credentials
+# Initialize Google Translate client
 translator = translate.Client(credentials=credentials)
 
 def translate(word, target_language):
     translation = translator.translate(word, target_language=target_language)
     return translation['translatedText']
-
